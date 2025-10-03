62c640085f54b5eda1733aefb725b476ace01c3d
Weyznnn <rafaninoadiyatma0989@gmail.com>
2025-10-03 08:14:40 +0700

    Modifikasi Percobaan 1

--- jobsheet5/ifCetakKRS24.java
+++ jobsheet5/ifCetakKRS24.java
@@ -7,13 +7,16 @@ public class ifCetakKRS24 {
         Scanner sc = new Scanner(System.in);
         boolean uktLunas;
         System.out.println("--- Cetak KRS SIAKAD ---");
-        System.out.println("Apakah UKT sudah lunas? (true/false): ");
+        System.out.print("Apakah UKT sudah lunas? (true/false): ");
         uktLunas = sc.nextBoolean();
         sc.close();
 
         if (uktLunas) {
             System.out.println("Pembayaran UKT terverifikasi");
             System.out.println("Silaka cetak KRS dan minta tanda tangan DPA");
+        }
+        else {
+            System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu");
         }
     }
 }
