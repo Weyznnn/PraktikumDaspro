7d12fc4c57435d07f0222f7ed7a5ed9019f6fcaf
Weyznnn <rafaninoadiyatma0989@gmail.com>
2025-10-03 09:51:02 +0700

    Tugas 1

--- jobsheet5/ifCetakKRS24.java
+++ jobsheet5/ifCetakKRS24.java
@@ -11,12 +11,16 @@ public class ifCetakKRS24 {
         uktLunas = sc.nextBoolean();
         sc.close();
 
-        if (uktLunas) {
+        String pesan = uktLunas ? "Pembayaran UKT terverifikasi\nSilakan cetak KRS dan minta tanda tangan DPA" : "Registrasi ditolak. Silakan lunasi UKT terlebih dahulu";
+
+        System.out.println(pesan);
+
+        /*if (uktLunas) {
             System.out.println("Pembayaran UKT terverifikasi");
-            System.out.println("Silaka cetak KRS dan minta tanda tangan DPA");
+            System.out.println("Silakan cetak KRS dan minta tanda tangan DPA");
         }
         else {
             System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu");
-        }
+        }*/
     }
 }
